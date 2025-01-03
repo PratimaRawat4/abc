97071d86f1221db0fdda63d71e9246a806eae27c
PratimaRawat4 <pritirawat2048@gmail.com>
2025-01-03 17:22:44 +0530

    first commit

--- my_project/settings.py
+++ my_project/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 import os
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-&gxro0e$g3aq9j5&1+=cn8(ld-kyvfmc9lm291*a#1+-t8z2mh'
-# SECRET_KEY=os.environ.get('SECRET_KEY')
+SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
